c126b3740eb80c33b40ffd97c720152c93e26fa3
ramune256 <shungo180620@outlook.jp>
2025-05-27 21:50:44 +0900

    amll fixes

--- ft_printf.c
+++ ft_printf.c
@@ -97,14 +97,22 @@ int ft_printf(const char *format, ...)
 	return (count);
 }
 
-// 後で消す
-int	main(void)
-{
-	int	char_count;
-	int	dummy;
+// // 後で消す
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	char_count1;
+// 	int	char_count2;
+// 	int	dummy;
+// 	dummy = 0;
 
-	dummy = 0;
-	char_count = ft_printf("Hello %s, %c, %d, %u, %x, %X, %p, %%, %i\n", "World", '!', 123, 456, 789, 1011, &dummy, 123);
-	ft_printf("char_count: %d\n", char_count);
-	return (0);
-}
+// 	//ft_printf
+// 	char_count1 = ft_printf("Hello %s, %c, %d, %u, %x, %X, %p, %%, %i\n", "World", '!', 123, 456, 789, 1011, &dummy, 123);
+// 	ft_printf("char_count1: %d\n", char_count1);
+
+// 	//printf
+// 	char_count2 = printf("Hello %s, %c, %d, %u, %x, %X, %p, %%, %i\n", "World", '!', 123, 456, 789, 1011, &dummy, 123);
+// 	printf("char_count2: %d\n", char_count2);
+
+// 	return (0);
+// }
