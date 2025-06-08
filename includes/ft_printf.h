ddb3f23ae3ec5a478c0ca36075276da376f62979
ramune256 <shungo180620@outlook.jp>
2025-06-08 22:00:18 +0900

    added a declaration

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 20:46:07 by shunwata          #+#    #+#             */
-/*   Updated: 2025/06/04 22:07:20 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/06/08 21:55:21 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putnbr_base(unsigned long long n, int base, int uppercase);
 int	ft_putnbr(int n);
+int	put_pointer(void *ptr);
 
 int	process_format(va_list args, char format);
 int	ft_printf(const char *format, ...);
